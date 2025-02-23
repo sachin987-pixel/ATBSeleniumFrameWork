0711a1e21716835a412a90503c67b36b6232feb9
sachin987-pixel <sachinmali0891@gmail.com>
2025-02-23 23:01:06 +0530

    initial commit

---
+++ src/test/java/Ecommerce/base/CommonToAllTest.java
@@ -0,0 +1,21 @@
+package Ecommerce.base;
+
+import Ecommerce.driver.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class CommonToAllTest {
+
+         @BeforeMethod
+    public void setUp(){
+             DriverManager.init();
+
+         }
+         @AfterMethod
+         public void tearDown(){
+             DriverManager.down();
+
+
+         }
+
+}
